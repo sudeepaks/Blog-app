a874b46405feb7970d273a32da2330d156928887
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-10-15 16:36:06 +0530

    added multer

--- api/index.js
+++ api/index.js
@@ -3,12 +3,20 @@ import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/user.js"
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
+const upload = multer({ dest: 'uploads/' })
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+res.status(200).json("Image has been uploaded")   
+} )
+
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
